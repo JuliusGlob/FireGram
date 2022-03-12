1693ca4c0400efcbd45c2380dbce75ef8e95e902
JuliusGlob <julio.robles@globant.com>
2022-03-12 20:58:13 -0300

    First Commit after deciding its always best to keep your repo around

---
+++ InstagramFirestore/InstagramFirestore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramFirestore
+//
+//  Created by Julio Ismael Robles on 11/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
